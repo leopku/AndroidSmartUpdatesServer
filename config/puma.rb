9a971abf3251a39601ad188c9f82dd80c078ab31
leopku <leopku@qq.com>
2015-02-11 14:17:02 +0800

    add configuration for host & port

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 # @Author: leopku
 # @Date:   2015-01-31 09:26:28
 # @Last Modified by:   leopku
-# @Last Modified time: 2015-02-06 16:43:48
+# @Last Modified time: 2015-02-11 14:15:41
 # environment ENV['RAILS_ENV']
 APP_ROOT = File.expand_path('../..', __FILE__)
 pidfile "#{APP_ROOT}/tmp/puma.pid"
@@ -14,6 +14,7 @@ prune_bundler
 
 restart_command 'bundle exec bin/puma'
 
+daemonize true
 workers 1
 threads 0,4
 preload_app!
